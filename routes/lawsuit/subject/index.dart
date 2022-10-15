cb3cba2f99e88303abd3a62310476c20297aba07
jakub-stefaniak <jakub.stefaniak@unitedideas.pl>
2022-10-15 16:56:04 +0200

    fix(lawsuit/subject): use correct lawsuit subjects

--- routes/lawsuit/subject/index.dart
+++ routes/lawsuit/subject/index.dart
@@ -3,26 +3,44 @@ import 'package:dart_frog/dart_frog.dart';
 final subjects = [
   {
     'id': 0,
-    'title': 'Prawa niemajątkowe',
+    'title': 'Prawa niemajątkowe z roszczeniami majątkowymi',
     'body':
-        'łącznie z nimi dochodzone roszczenia majątkowe oprócz spraw o ustalenie lub zaprzeczenie pochodzenia dziecka, o ustalenie bezskuteczności uznania ojcostwa oraz o rozwiązanie przysposobienia',
+        'Art. 17 pkt 1 kpc - za wyjątkiem spraw o ustalenie lub zaprzeczenie pochodzenia dziecka, o ustalenie bezskuteczności uznania ojcostwa oraz o rozwiązanie przysposobienia',
   },
   {
     'id': 1,
     'title': 'Roszczenia wynikające z Prawa prasowego',
-    'body': '',
+    'body':
+        'Art. 17 pkt 3 kpc - na gruncie ustawy Prawo prasowe tj. z dnia 14 września 2018 r. (Dz.U. z 2018 r. poz. 1914)',
   },
   {
     'id': 2,
-    'title': 'Prawa majątkowe',
+    'title': 'Prawa majątkowe - WPS powyżej 75.000,00 PLN',
     'body':
-        'kiedy wartość przedmiotu sporu przewyższa siedemdziesiąt pięć tysięcy złotych, oprócz spraw o alimenty, o naruszenie posiadania, o ustanowienie rozdzielności majątkowej między małżonkami, o uzgodnienie treści księgi wieczystej z rzeczywistym stanem prawnym oraz spraw rozpoznawanych w elektronicznym postępowaniu upominawczym',
+        'Art. 17 pkt 4 kpc - oprócz spraw o alimenty, o naruszenie posiadania, o ustanowienie rozdzielności majątkowej między małżonkami, o uzgodnienie treści księgi wieczystej z rzeczywistym stanem prawnym oraz spraw rozpoznawanych w elektronicznym postępowaniu upominawczym',
   },
   {
     'id': 3,
-    'title': 'Prawa majątkowe',
+    'title': 'Wydanie orzeczenia zastępującego uchwałę o podziale spółdzielni',
+    'body': 'Art. 17 pkt 41 kpc - jako podstawa roszczenia',
+  },
+  {
+    'id': 4,
+    'title': 'Dotyczące uchwał organów',
     'body':
-        'kiedy wartość przedmiotu sporu przewyższa siedemdziesiąt pięć tysięcy złotych, oprócz spraw o alimenty, o naruszenie posiadania, o ustanowienie rozdzielności majątkowej między małżonkami, o uzgodnienie treści księgi wieczystej z rzeczywistym stanem prawnym oraz spraw rozpoznawanych w elektronicznym postępowaniu upominawczym',
+        'Art. 17 pkt 42 kpc - dotyczących osób prawnych lub jednostek organizacyjnych niebędących osobami prawnymi, którym ustawa przyznaje zdolność prawną - w zakresie ich uchylenia, stwierdzenia nieważności, ustalenia istnienia lub nieistnienia tych uchwał',
+  },
+  {
+    'id': 5,
+    'title': 'Prawomocne, niezgodne z prawem orzeczenie',
+    'body':
+        'Art. 17 pkt 44 kpc - w zakresie odszkodowania z tytułu poniesionej szkody wyrządzonej tym orzeczeniem',
+  },
+  {
+    'id': 6,
+    'title': 'Ochrona danych osobowych',
+    'body':
+        'Art. 17 pkt 45 kpc - wszelkie roszczenia z zakresu przepisów o ochronie danych osobowych',
   }
 ];
 
